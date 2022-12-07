7af38d79e7c9aec9cda88945e46a6b60e6cf27be
berra592 <93202100+berra592@users.noreply.github.com>
2022-12-07 22:52:13 +0300

    ilk commit

---
+++ OgrenciWeb/src/main/java/tr/edu/medipol/yova/OgrenciWeb/OgrenciWebApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yova.OgrenciWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgrenciWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgrenciWebApplication.class, args);
+	}
+
+}
---
+++ OgrenciWeb/src/test/java/tr/edu/medipol/yova/OgrenciWeb/OgrenciWebApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.medipol.yova.OgrenciWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgrenciWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
